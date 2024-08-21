64f9e22a5afbefb5990661045ae8ea1e125ea47f
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-08-21 12:32:54 +0200

    [FullWidth] Remove all padding for small screens

--- app/components/FullWidth.tsx
+++ app/components/FullWidth.tsx
@@ -1,10 +1,11 @@
 import clsx from 'clsx';
 
-export default function FullWidth({ children, fullWidth = true }: { children: React.ReactNode, fullWidth?: boolean }) {
+export default function FullWidth({ children, fullWidth = true, padding = true }: { children: React.ReactNode, fullWidth?: boolean, padding?: boolean }) {
 
   return (
     <div className={clsx(
-              fullWidth ? 'relative w-screen ml-[-50vw] left-1/2 px-1 sm:px-4 md:px-8 lg:px-12 xl:px-24 shadow-2xl' : 'w-full'
+              fullWidth ? 'relative w-screen ml-[-50vw] left-1/2' : 'w-full',
+              padding && 'sm:px-4 md:px-8 lg:px-12 xl:px-24'
             )}>
       {children}
     </div>
