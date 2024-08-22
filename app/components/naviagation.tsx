7e0e04b4b6086fd85b9c716232a328572de5acc4
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-08-22 16:26:33 +0200

    Display currently selected link

--- app/components/naviagation.tsx
+++ app/components/naviagation.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import ThemeToggle from '@/app/ui/ToggleTheme';
 import Link from 'next/link';
 import { AcademicCapIcon, Square2StackIcon } from '@heroicons/react/24/outline'; 
 import { teko, prozaLibre } from '@/app/ui/fonts';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 export default function Navigation() {
+  const currentPath = usePathname();
+
   // Todo get current page path and highlight it if it matches one of the url
   const links = [
     { label: 'Portfolio', href: '/', icon: AcademicCapIcon },
@@ -15,12 +21,16 @@ export default function Navigation() {
       {
       links.map((link) => { 
         const IconComponent = link.icon;
+        const isCurrentPath = link.href === currentPath;
         return ( 
           <>
-            <div className="flex items-center rounded-lg overflow-hidden
-            text-gray-600 hover:text-gray-800 hover:bg-gray-300 
-            dark:text-gray-400 dark:hover:text-gray-100 dark:hover:bg-gray-700
-            mr-1 md:mr-2 lg:mr-4 px-2 py-2">
+            <div className={
+            clsx(
+            "flex items-center rounded-lg overflow-hidden",
+            isCurrentPath ? "text-gray-800 bg-gray-300 dark:text-gray-100 dark:bg-gray-700" 
+                          : "text-gray-600 hover:text-gray-800 hover:bg-gray-300 dark:text-gray-400 dark:hover:text-gray-100 dark:hover:bg-gray-700",
+             "mr-1 md:mr-2 lg:mr-4 px-2 py-2"
+              )}>
               <IconComponent className="w-5 h-5 md:w-7 md:h-7 mr-1 md:mr-2" />
               <Link key={link.href} href={link.href} 
               className={`${prozaLibre.className}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+"use client";
 import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Portfolio | Arnaud TANGUY',
-};
+import { motion } from 'framer-motion';
+//
+// export const metadata: Metadata = {
+//   title: 'Portfolio | Arnaud TANGUY',
+// };
 
 export default function Page() {
   return (
     <>
-      <div>
+      <div className="w-full flex items-center justify-center">
+      <motion.div className="w-1/2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
         <h1 className="font-bold text-3xl text-center m-4">Portfolio</h1>
+      </motion.div>
       </div>
     </>
   );
