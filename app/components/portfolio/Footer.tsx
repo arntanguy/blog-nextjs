ecc282be97402bd9189fee092080c46edbe0f8f5
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-08-23 19:18:47 +0200

    [portfolio] Margin with footer

--- app/components/portfolio/Footer.tsx
+++ app/components/portfolio/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-solid border-gray-300">
+    <footer className="w-full mt-64 border-t-2 border-solid border-gray-300">
       <div className="flex justify-center items-center py-8">
       <span>&copy; {new Date().getFullYear()} Arnaud TANGUY. All rights reserved.</span>
       </div>
