4d72bb11f9b7a88718f2c2f108227cdbbcad3b2a
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-09-04 13:24:15 +0200

    Nicer light keywords

--- app/components/portfolio/CVKeywords.tsx
+++ app/components/portfolio/CVKeywords.tsx
@@ -8,7 +8,7 @@ export default function Keywords( { keywords }: { keywords: string[] })
     <div className="flex flex-wrap gap-2 my-2">
       {
         uniqueKeywords.map((keyword) => (
-          <div key={keyword} className="flex items-center justify-center p-1 md:p-2 mr-0.5 md:m-1 text-sm md:text-base md:tracking-wider text-primary dark:text-gray-400 uppercase border-2 border-solid border-gray-500 rounded-lg hover:dark:border-gray-400 hover:dark:text-gray-300">{keyword}</div>
+          <div key={keyword} className="flex items-center justify-center p-1 md:p-2 mr-0.5 md:m-1 text-sm md:text-base md:tracking-wider text-gray-600 hover:text-gray-800 dark:text-gray-400 uppercase border-2 border-solid border-gray-500 hover:border-gray-800 hover:dark:border-gray-400 hover:dark:text-gray-300 rounded-lg">{keyword}</div>
         ))
       }
     </div>
