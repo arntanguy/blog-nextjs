82d6bfe4af8016fbb87e29decdb74c194dde8527
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-02-05 15:57:24 +0100

    [deploy] Tell nextjs to serve static files from the image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  // This line tells Next.js to serve files from the public folder
+  // even in standalone mode
+  compress: true
 }
 
 export default nextConfig;
