52c0bac603731c2249e7a4218e75a9da6cb26aa8
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-02-05 18:54:06 +0100

    [wip] Try disabling image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   output: 'standalone',
   // This line tells Next.js to serve files from the public folder
   // even in standalone mode
-  compress: true
+  compress: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig;
